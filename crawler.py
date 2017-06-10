7efd3d1996010057607866aa50fad212ecad83fa
ztztztzt <ztztztzt@umich.edu>
2017-06-10 01:11:43 -0400

    second try

--- crawler.py
+++ crawler.py
@@ -8,32 +8,45 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 if __name__ == '__main__':
-    imgs = [];
-    for i in range(5):
-        #url = 'https://www.qiushibaike.com/imgrank/
-        url = 'http://www.dbmeinv.com/dbgroup/show.htm?cid=2'
-        url = url + '&pager_offset=%s' %i
+
+
+    for i in range(1): #number of pages of travel blog
+
+        url = 'http://360.mafengwo.cn/travels/'
         res = requests.get(url)
         res.encoding = 'utf-8'
         soup = BeautifulSoup(res.text, 'html.parser')
-        imgs_i = soup.find_all("img")
-        imgs.extend(imgs_i)
+        t2 = soup.find_all('div',class_="itemBd")
+        for j in range(len(t2)):
 
-    _path = os.getcwd()
-    new_path = os.path.join(_path , 'try1_photos')
-    if not os.path.isdir(new_path):
-        os.mkdir(new_path)
-    new_path += '\ '
+            new_url = t2[j].a.get('href') #go to the specific blog
+            new_url = "http://360.mafengwo.cn" + new_url
+            res = requests.get(new_url)
+            res.encoding = 'utf-8'
+            soup = BeautifulSoup(res.text, 'html.parser')
+            imgs = soup.find_all("img")
+            loc = soup.find_all('div',class_="crumb")[0]
+            loc = loc.find_all("a")[1].text
+            author = soup.find_all('div',class_="author")[0]
+            author = author.p.text
+            author = author.split()[0]
+            _path = os.getcwd()
+            #set up the folder
+            new_path = os.path.join(_path , '%s_%s' %(author, loc))
+            if not os.path.isdir(new_path):
+                os.mkdir(new_path)
+            new_path += '/'
 
-    x = 1
-    if imgs == []:
-        print "Done!"
-    for img in imgs:
-        link = img.get('src')
-        print "It's downloading %s" %x + "th's piture"
-        #link = 'http:' + link
-        urllib.urlretrieve(link, new_path + '%s.jpg' %x)
-        x += 1
+            x = 1
+            if imgs == []:
+                print "Done!"
+            for img in imgs:
+                link = img.get('src')
+                if "http" in link:
+                    print "It's downloading %s" %x + "th's piture"
+                    #link = 'http:' + link
+                    urllib.urlretrieve(link, new_path + '%s.jpg' %x)
+                    x += 1
 
 
 
