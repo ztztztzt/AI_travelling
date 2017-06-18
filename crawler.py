9eb61f81f2f2b5930272c4e5e8aa78ffb6154fae
ztztztzt <ztztztzt@umich.edu>
2017-06-18 07:11:15 -0400

    update

--- crawler.py
+++ crawler.py
@@ -1,52 +1,67 @@
 #coding = 'utf-8'
 import requests
 from bs4 import BeautifulSoup
-import urllib
 import os
+import socket
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
+socket.setdefaulttimeout(2.0)
+import urllib
 
 if __name__ == '__main__':
 
 
-    for i in range(1): #number of pages of travel blog
 
-        url = 'http://360.mafengwo.cn/travels/'
-        res = requests.get(url)
+    url = 'http://www.mafengwo.cn/u/5428847/note.html'
+    res = requests.get(url)
+    res.encoding = 'utf-8'
+    soup = BeautifulSoup(res.text, 'html.parser')
+    author = soup.find_all('span',class_="MAvaName")[0]
+    author = author.text
+    article = soup.find_all('div',class_="note_info")
+    _path = os.getcwd()
+    new_path = os.path.join(_path , '%s' %(author))
+    if not os.path.isdir(new_path):
+        os.mkdir(new_path)
+
+    for j in range(len(article)):
+
+        new_url = article[j].find_all("a") #go to the specific blog
+        if (len(new_url) == 1):
+            new_url = new_url[0].get("href")
+        else:
+            new_url = new_url[1].get("href")
+
+        new_url = "http://www.mafengwo.cn" + new_url
+        res = requests.get(new_url)
         res.encoding = 'utf-8'
         soup = BeautifulSoup(res.text, 'html.parser')
-        t2 = soup.find_all('div',class_="itemBd")
-        for j in range(len(t2)):
+        imgs = soup.find_all("img")
+        time = soup.find_all('li',class_="time")
+        time = time[0].text.split('/')[1]
+        loc = soup.find_all('div',class_="relation_mdd opacity_on special_mdd")[0]
+        loc = loc.a.get("title")
+        #set up the folder
+        new_path_j = os.path.join(new_path , '%s_%s' %(loc, time))
+        if not os.path.isdir(new_path_j):
+            os.mkdir(new_path_j)
+        new_path_j += '/'
 
-            new_url = t2[j].a.get('href') #go to the specific blog
-            new_url = "http://360.mafengwo.cn" + new_url
-            res = requests.get(new_url)
-            res.encoding = 'utf-8'
-            soup = BeautifulSoup(res.text, 'html.parser')
-            imgs = soup.find_all("img")
-            loc = soup.find_all('div',class_="crumb")[0]
-            loc = loc.find_all("a")[1].text
-            author = soup.find_all('div',class_="author")[0]
-            author = author.p.text
-            author = author.split()[0]
-            _path = os.getcwd()
-            #set up the folder
-            new_path = os.path.join(_path , '%s_%s' %(author, loc))
-            if not os.path.isdir(new_path):
-                os.mkdir(new_path)
-            new_path += '/'
+        x = 1
+        if imgs == []:
+            print "Done!"
+        for img in imgs:
+            link = img.get('data-rt-src')
+            if link and "http" in link:
+                print "It's downloading %s" %x + "th's piture"
+                #link = 'http:' + link
 
-            x = 1
-            if imgs == []:
-                print "Done!"
-            for img in imgs:
-                link = img.get('src')
-                if "http" in link:
-                    print "It's downloading %s" %x + "th's piture"
-                    #link = 'http:' + link
-                    urllib.urlretrieve(link, new_path + '%s.jpg' %x)
-                    x += 1
+                try:
+                    urllib.urlretrieve(link, new_path_j + '%s.jpg' %x)
+                except:
+                    pass
+                x += 1
 
 
 
--- face_detection.py
+++ face_detection.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 import matplotlib.pyplot as plt  
 
 # rectangle color and stroke
@@ -7,9 +8,10 @@ strokeWeight = 1        # thickness of outline
 
 # set window name
 windowName = "Object Detection"
+i = 0
 
 # load an image to search for faces
-img = cv2.imread("5.jpg")
+img = cv2.imread("17.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # load detection file (various files for different views and uses)
 facecascade = cv2.CascadeClassifier("/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml")
@@ -19,12 +21,26 @@ facecascade = cv2.CascadeClassifier("/usr/local/share/OpenCV/haarcascades/haarca
 # preprocessing, as suggested by: http://www.bytefish.de/wiki/opencv/object_detection
 
 # detect objects, return as list
-faces = facecascade.detectMultiScale(gray) 
+faces = facecascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=9, minSize=(50,50))
 
 
     # get a list of rectangles
 for x,y, width,height in faces:
     cv2.rectangle(img, (x,y), (x+width, y+height), color, strokeWeight)
 
+# save 
+_path = os.getcwd()
+save_path = os.path.join(_path , "selfphotos")
+if not os.path.isdir(save_path):
+    os.mkdir(save_path)
+
+if not faces is (): 
+        for x,y,z,w in faces:
+            roiImg = img[y:y+w,x:x+z]
+            cv2.imwrite(save_path+'/' + str(i)+'.jpg',roiImg)
+            cv2.rectangle(img,(x,y),(x+z,y+w),(0,0,255),2)
+            i +=1
+
 # display!
+cv2.destroyAllWindows()
 cv2.imshow(windowName, img)
